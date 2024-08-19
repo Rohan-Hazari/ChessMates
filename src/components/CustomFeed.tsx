8f00eaa14e9c637c20db6b929e4e489121929869
Rohan-Hazari <hazarirohan2002@gmail.com>
2024-08-19 13:33:04 +0530

    Custom for signed in and General feed added for not logged in user added

---
+++ src/components/CustomFeed.tsx
@@ -0,0 +1,45 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULT } from "@/config"
+import { db } from "@/lib/db"
+import PostFeed from "./PostFeed"
+import { getAuthSession } from "@/lib/auth"
+
+const CustomFeed = async () => {
+
+    const session = await getAuthSession()
+
+    const followedCommunities = await db.subscription.findMany({
+        where: {
+            userId: session?.user.id
+        },
+        include: {
+            community: true
+        }
+    })
+    console.log("Followed Communities = ", followedCommunities);
+
+
+    const posts = await db.post.findMany({
+        where: {
+            community: {
+                name: {
+                    in: followedCommunities.map(({ community }) => community.id),
+                }
+            }
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+
+        include: {
+            votes: true,
+            author: true,
+            comments: true,
+            community: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULT,
+    })
+
+    return <PostFeed initialPosts={posts} />
+}
+
+export default CustomFeed
---
+++ src/components/GeneralFeed.tsx
@@ -0,0 +1,23 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULT } from "@/config"
+import { db } from "@/lib/db"
+import PostFeed from "./PostFeed"
+
+const GeneralFeed = async () => {
+
+    const posts = await db.post.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+        include: {
+            votes: true,
+            author: true,
+            comments: true,
+            community: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULT,
+    })
+
+    return <PostFeed initialPosts={posts} />
+}
+
+export default GeneralFeed
