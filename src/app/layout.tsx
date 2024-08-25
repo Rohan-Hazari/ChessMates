1c2a8c0b9a666cdcdab6090c5340fad537c460c3
Rohan-Hazari <hazarirohan2002@gmail.com>
2024-08-26 01:20:35 +0530

    ui bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           {/* @ts-expect-error Async Server Component */}
           <Navbar />
 
-          <div className="container max-w-7xl mx-auto h-full pt-12 ml-6 sm:">
+          <div className="container max-w-7xl mx-auto h-full pt-12 ">
             {children}
           </div>
 
