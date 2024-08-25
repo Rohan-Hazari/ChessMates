4e49aaa0b808279e069117668b475a276c754ee0
Rohan-Hazari <hazarirohan2002@gmail.com>
2024-08-25 23:07:30 +0530

    Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Toaster from "@/components/ui/Toaster";
 import Providers from "@/components/Providers";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata = {
   title: "ChessMates",
@@ -14,10 +15,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  authModal,
 }: {
   children: React.ReactNode;
-  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -26,11 +25,12 @@ export default function RootLayout({
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased ">
         <Providers>
+          {/* responsive bottom bar for mobile and styling remains  */}
+          {/* <Sidebar /> */}
           {/* @ts-expect-error Async Server Component */}
           <Navbar />
-          {authModal}
 
-          <div className="container max-w-7xl mx-auto h-full pt-12 ">
+          <div className="container max-w-7xl mx-auto h-full pt-12 ml-6 sm:">
             {children}
           </div>
 
