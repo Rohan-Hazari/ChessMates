543bb7386bc6aa1b65a6f6859ee959036d3ac1aa
Rohan-Hazari <hazarirohan2002@gmail.com>
2025-04-29 02:10:57 +0530

    Fix: Build error fixes

--- src/components/news/NewsFeed.tsx
+++ src/components/news/NewsFeed.tsx
@@ -10,10 +10,12 @@ const NewsFeed: FC<NewsFeedProps> = ({ initialNews }) => {
   return (
     <div className="mt-6">
       <div className="container mx-auto py-8 px-4">
-        <h1 className="text-3xl font-bold mb-8">Today's Headlines</h1>
+        <h1 className="text-3xl font-bold mb-8">Today &apos s Headlines</h1>
         <div className="space-y-6">
           {initialNews.map((news: TranslatedNews) => (
-            <NewsPost news={news} />
+            <div key={news.id}>
+              <NewsPost news={news} />
+            </div>
           ))}
         </div>
       </div>
